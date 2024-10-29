1db31a4065e11531ff633c4d4c2c328505370ff9
begumkaya10 <147401559+begumkaya10@users.noreply.github.com>
2024-10-29 16:58:16 +0300

    Basic FlappyBird
    Bu projeyi, popüler Flappy Bird oyununu basit bir versiyonu olarak oluşturdum. Oyuncular, kuşu engellerden kaçırarak en yüksek puanı almaya çalışıyorlar.
    
    Özellikler
    Basit ve eğlenceli oynanış
    Kolay kontrol mekanizması
    Farklı zorluk seviyeleri
    Kullanım
    Projeyi klonlayın veya indirin.
    Gerekli kütüphaneleri yükleyin.
    Oyunu başlatın ve kuşu yönlendirin!

---
+++ FlappyBirdGame/FlappyBirdGame/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace FlappyBirdGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox_Bird = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Top = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Ground = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Bottom = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer_GameControl = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bottom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_Bird
+            // 
+            this.pictureBox_Bird.Image = global::FlappyBirdGame.Properties.Resources.bird;
+            this.pictureBox_Bird.Location = new System.Drawing.Point(105, 304);
+            this.pictureBox_Bird.Name = "pictureBox_Bird";
+            this.pictureBox_Bird.Size = new System.Drawing.Size(100, 104);
+            this.pictureBox_Bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Bird.TabIndex = 0;
+            this.pictureBox_Bird.TabStop = false;
+            // 
+            // pictureBox_Top
+            // 
+            this.pictureBox_Top.Image = global::FlappyBirdGame.Properties.Resources.pipes__1_;
+            this.pictureBox_Top.Location = new System.Drawing.Point(437, -2);
+            this.pictureBox_Top.Name = "pictureBox_Top";
+            this.pictureBox_Top.Size = new System.Drawing.Size(123, 237);
+            this.pictureBox_Top.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Top.TabIndex = 0;
+            this.pictureBox_Top.TabStop = false;
+            this.pictureBox_Top.Click += new System.EventHandler(this.pictureBox_Top_Click);
+            // 
+            // pictureBox_Ground
+            // 
+            this.pictureBox_Ground.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Ground.Image")));
+            this.pictureBox_Ground.Location = new System.Drawing.Point(0, 717);
+            this.pictureBox_Ground.Name = "pictureBox_Ground";
+            this.pictureBox_Ground.Size = new System.Drawing.Size(1243, 109);
+            this.pictureBox_Ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Ground.TabIndex = 0;
+            this.pictureBox_Ground.TabStop = false;
+            // 
+            // pictureBox_Bottom
+            // 
+            this.pictureBox_Bottom.Image = global::FlappyBirdGame.Properties.Resources.pipes;
+            this.pictureBox_Bottom.Location = new System.Drawing.Point(432, 515);
+            this.pictureBox_Bottom.Name = "pictureBox_Bottom";
+            this.pictureBox_Bottom.Size = new System.Drawing.Size(128, 208);
+            this.pictureBox_Bottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Bottom.TabIndex = 0;
+            this.pictureBox_Bottom.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(49, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " ";
+            // 
+            // timer_GameControl
+            // 
+            this.timer_GameControl.Enabled = true;
+            this.timer_GameControl.Interval = 20;
+            this.timer_GameControl.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1080, 825);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox_Top);
+            this.Controls.Add(this.pictureBox_Bird);
+            this.Controls.Add(this.pictureBox_Ground);
+            this.Controls.Add(this.pictureBox_Bottom);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bottom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_Bird;
+        private System.Windows.Forms.PictureBox pictureBox_Top;
+        private System.Windows.Forms.PictureBox pictureBox_Ground;
+        private System.Windows.Forms.PictureBox pictureBox_Bottom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer_GameControl;
+    }
+}
+
---
+++ FlappyBirdGame/FlappyBirdGame/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBirdGame
+{
+    public partial class Form1 : Form
+    {
+        // Oyun değişkenleri
+        int pipeSpeed = 8;           // Boru hızı
+        int gravity = 5;             // Yerçekimi kuvveti
+        int jumpForce = -15;         // Kuşun zıplama kuvveti
+        int score = 0;               // Puan
+        //int gap = 150;             // Borular arasındaki boşluk
+        Random rand = new Random();  // Rastgele sayılar için
+        bool isGameOver = false;     // Oyun durumu
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        // Oyun başlangıç ayarları
+        public void StartGame()
+        {
+            score = 0;
+            gravity = 5;
+            isGameOver = false;
+            label1.Text = "SCORE: " + score;
+
+            pictureBox_Bird.Top = 200;
+            pictureBox_Bottom.Left = this.ClientSize.Width;
+            pictureBox_Top.Left = this.ClientSize.Width + 100;
+
+            RandomizePipes();
+            timer_GameControl.Start();
+        }
+
+        // Oyun zamanlayıcısına bağlı metod (her tick'te çağrılır)
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            // Kuşun yerçekimi etkisiyle hareketi
+            if (pictureBox_Bird.Top + pictureBox_Bird.Height > pictureBox_Ground.Top)
+            {
+                pictureBox_Bird.Top = pictureBox_Ground.Top - pictureBox_Bird.Height;
+                EndGame();
+            }
+            else
+            {
+                pictureBox_Bird.Top += gravity;
+            }
+
+            // Boruları sola hareket ettir
+            pictureBox_Bottom.Left -= pipeSpeed;
+            pictureBox_Top.Left -= pipeSpeed;
+
+            // Skoru güncelle
+            label1.Text = "SCORE: " + score;
+
+            // Borular ekranın solundan çıkarsa yeniden konumlandır
+            if (pictureBox_Bottom.Left < -150)
+            {
+                pictureBox_Bottom.Left = this.ClientSize.Width;
+                RandomizePipes();
+                score++;
+            }
+            if (pictureBox_Top.Left < -180)
+            {
+                pictureBox_Top.Left = this.ClientSize.Width + 100;
+                RandomizePipes();
+                score++;
+            }
+
+            // Çarpma kontrolü
+            if (pictureBox_Bird.Bounds.IntersectsWith(pictureBox_Bottom.Bounds) ||
+                pictureBox_Bird.Bounds.IntersectsWith(pictureBox_Top.Bounds) ||
+                pictureBox_Bird.Bounds.IntersectsWith(pictureBox_Ground.Bounds) ||
+                pictureBox_Bird.Top < 0)
+            {
+                EndGame();
+            }
+
+            // Skor belirli bir seviyeyi geçerse boru hızı artırılır
+            if (score > 30)
+            {
+                pipeSpeed = 15;
+            }
+        }
+
+        // Rastgele boru yüksekliklerini ayarla
+        public void RandomizePipes()
+        {
+            int gap = 200; // Kuşun geçebileceği sabit boşluk
+            int pipeMinHeight = 30; // Minimum üst boru yüksekliği
+            int pipeMaxHeight = pictureBox_Ground.Top - gap - pipeMinHeight;
+
+            // Üst borunun yüksekliğini rastgele ayarla
+            int randomTopHeight = rand.Next(pipeMinHeight, pipeMaxHeight);
+            pictureBox_Top.Height = randomTopHeight;
+            pictureBox_Top.Top = 0;
+
+            // Alt borunun yüksekliğini ve pozisyonunu ayarla
+            pictureBox_Bottom.Height = pictureBox_Ground.Top - randomTopHeight - gap;
+            pictureBox_Bottom.Top = pictureBox_Ground.Top - pictureBox_Bottom.Height;
+        }
+
+        // Oyun bittiğinde yapılacak işlemler
+        public void EndGame()
+        {
+            timer_GameControl.Stop();
+            label1.Text = "GAME OVER! Press R to Restart";
+            isGameOver = true;
+        }
+
+        // Oyunu yeniden başlat
+        public void RestartGame()
+        {
+            StartGame();
+        }
+
+        // Klavyeden tuşa basıldığında yapılacak işlemler
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            // Boşluk tuşuna basıldığında kuşu zıplat
+            if (e.KeyCode == Keys.Space && !isGameOver)
+            {
+                gravity = jumpForce;
+            }
+
+            // "R" tuşuna basıldığında oyunu yeniden başlat
+            if (e.KeyCode == Keys.R && isGameOver)
+            {
+                RestartGame();
+            }
+        }
+
+        // Klavyeden tuş bırakıldığında yapılacak işlemler
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            // Boşluk tuşu bırakıldığında yerçekimi tekrar devreye girer
+            if (e.KeyCode == Keys.Space && !isGameOver)
+            {
+                gravity = 5;
+            }
+        }
+
+        private void pictureBox_Top_Click(object sender, EventArgs e)
+        {
+            // Bu alan gerektiğinde eklenebilir.
+        }
+    }
+}
+
